06d32d26d3655ff2bca5b407a93eff7fae007901
Prabej Hussain <contact.prabej@gmail.com>
2024-06-02 21:42:55 +0545

    Add PORT

--- index.js
+++ index.js
@@ -64,7 +64,7 @@ app.post('/login',async(req,res)=>{
     })
 })
 
-const PORT = process.env.PORT || 5000 ;
+const PORT = 5000 ;
 app.listen(PORT, () => {
     console.log(`Server is running at ${PORT}.`)
 });
